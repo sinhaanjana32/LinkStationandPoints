5d855052bb5c71316e4c52f767ed57b3a95b2843
Anjana Sinha <62407714+sinhaanjana32@users.noreply.github.com>
2020-10-05 10:43:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,108 +5,103 @@ import Chart from "./Chart";
 class App extends Component {
 constructor(props) {
 super(props);
+
+  {/* Initialization of state*/}
+
 this.state = {
-Mobile: [
+Device: [
 { x: 0, y: 0 },
 { x: 100, y: 100 },
 { x: 15, y: 10 },
 { x: 18, y: 18 },
 ],
-powerStation: [
+linkStation: [
 { x: 0, y: 0, r: 10 },
 { x: 20, y: 20, r: 5 },
 { x: 10, y: 0, r: 12 },
 ],
 
 chartData: {},
-
-bestStation: [
-{
-jx: 0,
-jy: 0,
-mx: 0,
-my: 0,
-
-power: 0,
-},
-],
-
-unreachablePoints: [
-{
-ix: 0,
-iy: 0,
-},
-],
+bestStation: [],
+unreachablePoints: [],
 };
-
-console.log(this.state.Mobile.length);
-console.log(this.state.powerStation.length);
 }
 
+
+
+
 calculateDistance(dx, dy) {
-return Math.floor(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)));
+return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+}
+
+calulateP(distance, range){
+    if(range > distance){
+      return Math.pow(range - distance, 2);
+    } else {
+      return 0
+    }
 }
 
 mainCalculation() {
+var i, j;
+for(i=0; i<this.state.Device.length; i++){
+  let xp = 0; 
+  let yp =0;
+  let pMax =0;
+ 
+ for(j=0; j<this.state.linkStation.length; j++) {
 
-  let pow
-const result = this.state.powerStation.reduce(
-(acc, station) => {
-let distance1;
-this.state.Mobile.forEach((item) => {
-const dx =  station.x -item.x;
-const dy =  station.y- item.y;
-distance1 = this.calculateDistance(dx, dy);
-console.log(  item.x +','+ item.y + 'is'+ distance1)
+  const dx =  this.state.Device[i].x -this.state.linkStation[j].x;
+  const dy =  this.state.Device[i].y- this.state.linkStation[j].y;
+  const range = this.state.linkStation[j].r
 
+  var distance = this.calculateDistance(dx, dy);
+  var p = this.calulateP(distance,range)
 
-if (station.r > distance1) {
-pow = Math.pow(station.r - distance1, 2);
+  if(p > pMax){
+    pMax = p;
+    xp = this.state.linkStation[j].x
+    yp = this.state.linkStation[j].y
+  } 
+   }
+ if(pMax> 0){
+  var newBestStation = this.state.bestStation
+  newBestStation.push({
+    jx:this.state.Device[i].x,
+    jy: this.state.Device[i].y,
+    power:pMax,
+    mx: xp,
+    my: yp,
 
-acc.bestStation.push({
-power: pow,
-mx: station.x,
-my: station.y,
-jx: item.x,
-jy: item.y
-});
-return;
+  })
+  this.setState({bestStation : newBestStation})
+
+ }  else {
+
+  var   newunreachablePoints = this.state.unreachablePoints
+  newunreachablePoints.push({
+    ix: this.state.Device[i].x,
+    iy: this.state.Device[i].y,
+    });
+    this.setState({...this.state.unreachablePoints,...newunreachablePoints})
+ }
 }
-if (distance1 > station.r)  {
-  console.log(  item.x +','+ item.y + ' range is'+ distance1 +'gt'+  station.r)
-  
- pow = 0;
-  if (
-  acc.unreachablePoints.find(
-  (k) => k.ix === item.x && k.iy === item.y
-  ) === undefined
-  )
-  acc.unreachablePoints.push({
-  ix: item.x,
-  iy: item.y,
-  });
-} else return;
-
-});
-return acc;
-},
-{ bestStation: [], unreachablePoints: [] });
-this.setState({...this.state,...result})
 }
 
 
 
-renderItems() {
-console.log(this.state);
-return this.state.bestStation.map((items) => (
-<p style={{ color: "green" }}>
+renderReachableDevices() {
+return    this.state.bestStation.map((items) => (
+<p  style={{ color: "green" }}>
 {" "}
 Best Link station for {items.jx} {items.jy} is {items.mx}, {items.my}{" "}with power {items.power}
 </p>
 ));
 }
 
-render2nditems() {
+
+
+renderNonReachableDevices() {
 return this.state.unreachablePoints.map((points) => (
 <p style={{ color: "blue" }}>
 {" "}
@@ -115,9 +110,7 @@ No Link station within reach for [x: {points.ix} y:{points.iy}]
 ));
 }
 
-componentDidMount() {
-this.mainCalculation();
-}
+
 
 getChartData() {
 this.setState({
@@ -126,14 +119,14 @@ datasets: [
 {
 label: "Power Station",
 
-data: this.state.powerStation,
+data: this.state.linkStation,
 backgroundColor: "yellow",
 borderWidth: "4",
 borderColor: "grey",
 },
 {
-label: "Mobile",
-data: this.state.Mobile,
+label: "Device",
+data: this.state.Device,
 backgroundColor: "red",
 borderWidth: "1",
 borderColor: "black",
@@ -143,9 +136,14 @@ borderColor: "black",
 });
 }
 
+componentDidMount() {
+this.mainCalculation();
+}
+
 componentWillMount() {
 this.getChartData();
 }
+
 
 render() {
 return (
@@ -154,8 +152,9 @@ return (
 <h4 style={{ color: "tomato" }}>The Link Station & Points </h4>
 </div>
 <Chart chartData={this.state.chartData} />
-{this.renderItems()}
-{this.render2nditems()}
+  {/* rendering reachableDevices and nonReachableDevices seperately which will increase the scope of adding new Device and linkStation from Input*/}
+{this.renderReachableDevices()}
+{this.renderNonReachableDevices()}
 </div>
 );
 }
